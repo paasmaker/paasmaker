bd10c8700c275618899da900ef98ccc544c708d7
Daniel Foote <freefoote@dview.net>
2012-08-26 10:53:34 +0800

    Checkin of dotconf library, before making any changes/fixes to the code.

---
+++ dotconf/parser.py
@@ -0,0 +1,331 @@
+""" Dotconf lexer and parser.
+"""
+
+
+from glob import glob
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+from dotconf.tree import ConfigSection, ConfigValue, Position
+
+
+UNITS = {'k': 10 ** 3,
+         'M': 10 ** 6,
+         'G': 10 ** 9,
+         'T': 10 ** 12,
+         'P': 10 ** 15,
+         'E': 10 ** 18,
+         'Z': 10 ** 21,
+         'Y': 10 ** 24,
+         'Ki': 2 ** 10,
+         'Mi': 2 ** 20,
+         'Gi': 2 ** 30,
+         'Ti': 2 ** 40,
+         'Pi': 2 ** 50,
+         'Ei': 2 ** 60,
+         'Zi': 2 ** 70,
+         'Yi': 2 ** 80}
+
+
+class ParsingError(Exception):
+
+    """ Error raised when a parsing error occurs.
+    """
+
+    def __init__(self, msg, position=None):
+        super(ParsingError, self).__init__(msg)
+        self.position = position
+
+
+def default_external_opener(locator):
+    """ The default locator used to open included external files.
+    """
+    parsed_externals = []
+    for external in glob(locator):
+        try:
+            with open(external) as fexternal:
+                external_data = fexternal.read()
+        except IOError as err:
+            raise ParsingError('Unable to open %s (%s)' % (external, err))
+        parser = DotconfParser(external_data, debug=False, write_tables=False,
+                               errorlog=yacc.NullLogger(), input_name=external,
+                               external_opener=default_external_opener)
+        parsed_externals.append(parser.parse())
+    return parsed_externals
+
+
+#
+# Lexer
+#
+
+class DotconfLexer(object):
+
+    """ Lexer for the DotConf format.
+
+    :param \*\*kwargs: arguments to give to the ply lexer
+
+    Usage example::
+
+    >>> lexer = DotConfLexer()
+    >>> lexer.input('test { key = yes }')
+    >>> print lexer.next()
+    """
+
+    def __init__(self, **kwargs):
+        self._lexer = lex.lex(module=self, **kwargs)
+
+    #
+    # Tokens definition
+    #
+
+    reserved = {'yes': 'YES', 'no': 'NO', 'include': 'INCLUDE'}
+    reserved.update(dict(((kw, 'UNIT') for kw in UNITS)))
+    tokens = ['LBRACE', 'RBRACE', 'NAME', 'TEXT', 'NUMBER',
+              'ASSIGN', 'LIST_SEP'] + list(set(reserved.values()))
+
+    t_LBRACE = '{'
+    t_RBRACE = '}'
+    t_ASSIGN = '='
+    t_LIST_SEP = ','
+    t_ignore = ' \t'
+
+    def t_NAME(self, token):
+        r'[a-zA-Z_][a-zA-Z0-9_-]*'
+        token.type = self.reserved.get(token.value, 'NAME')
+        # Handle the boolean case:
+        if token.type == 'YES':
+            token.value = True
+        elif token.type == 'NO':
+            token.value = False
+        elif token.type == 'UNIT':
+            token.value = UNITS[token.value]
+        return token
+
+    def t_TEXT(self, token):
+        r'(["]([\\]["]|[^"]|)*["]|[\']([\\][\']|[^\'])*[\'])'
+        value = token.value[1:-1].replace('\\' + token.value[0], token.value[0])
+        token.value = value
+        # Count the lines in the string:
+        token.lexer.lineno += value.count('\n')
+        return token
+
+    def t_NUMBER(self, token):
+        r'[-+]?[0-9]+(\.[0-9]+)?'
+        if token.value.isdigit():
+            token.value = int(token.value)
+        else:
+            token.value = float(token.value)
+        return token
+
+    def t_EOL(self, token):
+        r'[\n]+'
+        token.lexer.lineno += len(token.value)
+
+    def t_COMMENT(self, token):
+        r'[#].*'
+
+    def t_error(self, token):
+        position = Position(self._input_name,
+                            self._lexer.lineno,
+                            self._lexer.lexpos)
+        raise ParsingError('Illegal character %r' % token.value[0], position)
+
+
+    #
+    # Public methods
+    #
+
+    def column(self, lexpos):
+        """ Find the column according to the lexpos.
+        """
+        # This code is taken from the python-ply documentation
+        # see: http://www.dabeaz.com/ply/ply.html section 4.6
+        last_cr = self._current_input.rfind('\n', 0, lexpos)
+        if last_cr < 0:
+            last_cr = 0
+        column = (lexpos - last_cr)
+        return column
+
+
+    #
+    # Bindings to the internal _lexer object
+    #
+
+    def input(self, input):
+        self._current_input = input
+        return self._lexer.input(input)
+
+    def __getattr__(self, name):
+        attr = getattr(self._lexer, name)
+        if attr is None:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self, name))
+        else:
+            return attr
+
+#
+# Parser
+#
+
+class DotconfParser(object):
+
+    """ Parser for the Dotconf format.
+    """
+
+    tokens = DotconfLexer.tokens
+
+    def __init__(self, input, **kwargs):
+        self._input = input
+        self._input_name = kwargs.pop('input_name', '<unknown>')
+        self._external_opener = kwargs.pop('external_opener',
+                                           default_external_opener)
+        self._lexer = kwargs.pop('lexer', DotconfLexer())
+        self._parser = yacc.yacc(module=self, **kwargs)
+        self._old_line = 0
+
+    def _check_line(self, current, lineno, pos, token):
+        if self._old_line == current:
+            pos = Position(self._input_name, lineno, pos)
+            raise ParsingError('Syntax error near of "%s", '
+                               'newline missing?' % token, pos)
+        else:
+            self._old_line = current
+
+    #
+    # Rules
+    #
+
+    start = 'top'
+
+    def p_top(self, p):
+        """top : section_content"""
+        section = ConfigSection('__top__')
+        for child in p[1]:
+            if isinstance(child, ConfigSection):
+                child.parent = section
+            section.register(child)
+        p[0] = section
+
+    def p_assignation(self, p):
+        """assignment : NAME ASSIGN value
+                      | NAME ASSIGN list"""
+        position = Position(self._input_name,
+                            p.lineno(3),
+                            p.lexer.column(p.lexpos(3)))
+        value = ConfigValue(p[1], p[3], position=position)
+        p[0] = value
+
+    def p_value(self, p):
+        """value : TEXT
+                 | YES
+                 | NO
+                 | number"""
+        p[0] = p[1]
+
+    def p_number(self, p):
+        """number : NUMBER
+                  | NUMBER UNIT"""
+        if len(p) == 2:
+            p[0] = p[1]
+        else:
+            p[0] = p[1] * p[2]
+
+    #
+    # List management rules:
+    #
+
+    def p_list(self, p):
+        """list : value LIST_SEP list_next
+                | value LIST_SEP"""
+        if len(p) == 3:
+            p[0] = [p[1]]
+        elif len(p) == 4:
+            p[3].insert(0, p[1])
+            p[0] = p[3]
+
+    def p_list_next(self, p):
+        """list_next : value
+                     | value LIST_SEP
+                     | value LIST_SEP list_next"""
+        if len(p) in (2, 3):
+            p[0] = [p[1]]
+        elif len(p) == 4:
+            p[3].insert(0, p[1])
+            p[0] = p[3]
+
+    #
+    # Sections:
+    #
+
+    def p_section_content_empty(self, p):
+        """section_content : empty"""
+        p[0] = []
+
+    def p_section_content_assignation(self, p):
+        """section_content : section_content assignment
+                           | section_content section"""
+        self._check_line(p.lexer.lineno, p.lineno(2),
+                         self._lexer.column(p.lexpos(2)), p[2].name)
+        p[1].append(p[2])
+        p[0] = p[1]
+
+    def p_section_content_include(self, p):
+        """section_content : section_content INCLUDE TEXT"""
+        for external in self._external_opener(p[3]):
+            p[1] += list(external.iterchildren())
+        p[0] = p[1]
+
+    def p_section(self, p):
+        """section : NAME LBRACE section_content RBRACE
+                   | NAME section_args LBRACE section_content RBRACE"""
+        name = p[1]
+        if len(p) == 5:
+            args = None
+            section_content = p[3]
+        else:
+            args = p[2]
+            section_content = p[4]
+        column = p.lexer.column(p.lexpos(1))
+        position = Position(self._input_name, p.lineno(1), column)
+        section = ConfigSection(name, args=args, position=position)
+        for child in section_content:
+            if isinstance(child, ConfigSection):
+                child.parent = section
+            section.register(child)
+        p[0] = section
+
+    def p_section_args(self, p):
+        """section_args : value LIST_SEP section_args
+                        | value"""
+        if len(p) == 2:
+            position = Position(self._input_name,
+                                p.lineno(1),
+                                p.lexer.column(p.lexpos(1)))
+            p[0] = ConfigValue('<args>', [p[1]], position=position)
+        else:
+            p[3].value.insert(0, p[1])
+            p[0] = p[3]
+
+    def p_empty(self, p):
+        """empty :"""
+        pass
+
+    def p_error(self, token):
+        column = self._lexer.column(token.lexpos)
+        pos = Position(self._input_name, token.lineno, column)
+        raise ParsingError('Syntax error near of "%s"' % token.value, pos)
+
+
+    #
+    # Bindings to the internel _parser object
+    #
+
+    def parse(self):
+        return self._parser.parse(self._input, self._lexer, tracking=True)
+
+    def __getattr__(self, name):
+        attr = getattr(self._parser, name)
+        if attr is None:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self, name))
+        else:
+            return attr
---
+++ dotconf/schema/containers.py
@@ -0,0 +1,329 @@
+""" Builtin containers of dotconf.schema
+"""
+
+try:
+    import argparse
+except ImportError:
+    argparse = None
+from itertools import izip
+
+from dotconf.tree import ConfigSection, ConfigValue
+from dotconf.schema import Container, ArgparseContainer, ValidationError
+
+
+required = object()
+many = (1, None)
+once = (1, 1)
+
+
+class Value(ArgparseContainer):
+
+    """ A value container used to store a scalar value of specified type.
+
+    :param value_type: the type of the value stored by container
+    :param default: the default value of the container
+    """
+
+    def __init__(self, value_type, default=required, **kwargs):
+        super(Value, self).__init__(**kwargs)
+        self._type = value_type
+        self._default = default
+
+    def populate_argparse(self, parser, name):
+        value = self
+        class Action(argparse.Action):
+            def __init__(self, **kwargs):
+                super(Action, self).__init__(**kwargs)
+                self._const = kwargs.get('const', None)
+            def __call__(self, parser, namespace, values, option_string=None):
+                if self._const is not None:
+                    value._argparse_value = ConfigValue(name, self._const)
+                else:
+                    value._argparse_value = ConfigValue(name, values[0])
+
+        if self._argparse_names:
+            if self._type.is_argparse_flag:
+                nargs = 0
+                const = True
+            else:
+                nargs = 1
+                const = None
+            nargs = 0 if self._type.is_argparse_flag else 1
+            parser.add_argument(*self._argparse_names, action=Action,
+                                type=self._type.cast, nargs=nargs,
+                                metavar=self._argparse_metavar,
+                                help=self._argparse_help, const=const)
+            if self._type.is_argparse_flag and self._argparse_names_invert:
+                parser.add_argument(*self._argparse_names_invert,
+                                    action=Action, type=self._type.cast,
+                                    nargs=0, help=self._argparse_help_invert,
+                                    const=False)
+
+    def validate(self, value):
+        if self._argparse_value is not None:
+            value = self._argparse_value
+        if value is None:
+            if self._default is required:
+                raise ValidationError('this value is required')
+            else:
+                return ConfigValue(None, self._default)
+        else:
+            if isinstance(value.value, list):
+                if len(value.value) == 1:
+                    value.value = value.value[0]
+                else:
+                    raise ValidationError('%r is a list' % value.value,
+                                          position=value.position)
+            try:
+                validated_value = self._type.validate(value.value)
+            except ValidationError as err:
+                raise ValidationError(str(err), position=value.position)
+            return ConfigValue(value.name, validated_value, position=value.position)
+
+
+class List(ArgparseContainer):
+
+    """ A list container used to store a list of scalar value of specified type.
+
+    :param values_type: type of values
+    :param default: the default value of the container
+    """
+
+    def __init__(self, values_type, default=required, **kwargs):
+        super(List, self).__init__(**kwargs)
+        self._type = values_type
+        self._default = default
+
+    def populate_argparse(self, parser, name):
+        value = self
+        class Action(argparse.Action):
+            def __call__(self, parser, namespace, values, option_string=None):
+                value._argparse_value = ConfigValue(name, values)
+        if self._argparse_names:
+            nargs = '*'
+            parser.add_argument(*self._argparse_names, action=Action,
+                                type=self._type.cast, nargs=nargs,
+                                metavar=self._argparse_metavar,
+                                help=self._argparse_help)
+
+    def validate(self, value):
+        if self._argparse_value is not None:
+            value = self._argparse_value
+        if value is None:
+            if self._default is required:
+                raise ValidationError('this value is required')
+            else:
+                return ConfigValue(None, self._default)
+        else:
+            values = value.value
+            if not isinstance(values, list):
+                values = [values]
+            validated_list = []
+            for i, item in enumerate(values):
+                try:
+                    item = self._type.validate(item)
+                except ValidationError as err:
+                    raise ValidationError('item #%d, %s' % (i, err),
+                                          position=value.position)
+                else:
+                    validated_list.append(item)
+            return ConfigValue(value.name, validated_list, position=value.position)
+
+
+class Array(List):
+
+    """ An array container used to store a fixed size list of scalar values of
+        the specified type.
+
+    :param size: size of the array
+    :param \*\*kwargs: same arguments as List
+    """
+
+    def __init__(self, size, *args, **kwargs):
+        super(Array, self).__init__(*args, **kwargs)
+        self._size = size
+
+    def populate_argparse(self, parser, name):
+        value = self
+        class Action(argparse.Action):
+            def __call__(self, parser, namespace, values, option_string=None):
+                value._argparse_value = ConfigValue(name, values)
+        if self._argparse_names:
+            parser.add_argument(*self._argparse_names, action=Action,
+                                type=self._type.cast, nargs=self._size,
+                                metavar=self._argparse_metavar,
+                                help=self._argparse_help)
+
+    def validate(self, value):
+        value = super(Array, self).validate(value)
+        if len(value.value) != self._size:
+            raise ValidationError('bad array size (should be %d, found %d items)'
+                                  % (self._size, len(value.value)))
+        return value
+
+
+class TypedArray(ArgparseContainer):
+
+    """ An array container used to store a fixed size list of scalar values
+        with specified type for each of them.
+
+    :param values_types: types of each item in a list
+    :param default: the default value of the container
+    """
+
+    def __init__(self, values_types, default=required, **kwargs):
+        super(TypedArray, self).__init__(**kwargs)
+        self._types = values_types
+        self._default = default
+
+    def populate_argparse(self, parser, name):
+        value = self
+        class Action(argparse.Action):
+            def __call__(self, parser, namespace, values, option_string=None):
+                value._argparse_value = ConfigValue(name, values)
+        if self._argparse_names:
+            nargs = '*'
+            parser.add_argument(*self._argparse_names, action=Action,
+                                type=str, nargs=nargs,
+                                metavar=self._argparse_metavar,
+                                help=self._argparse_help)
+
+    def validate(self, value):
+        if self._argparse_value is not None:
+            value = self._argparse_value
+        if value is None:
+            if self._default is required:
+                raise ValidationError('this value is required')
+            else:
+                return ConfigValue(None, self._default)
+        else:
+            values = value.value
+            if not isinstance(values, list):
+                values = [values]
+            validated_list = []
+            if len(values) != len(self._types):
+                raise ValidationError('bad array size (should be %d, found %d '
+                                      'items)' % (len(self._types), len(values)))
+
+            for i, (item, item_type) in enumerate(izip(values, self._types)):
+                try:
+                    item = item_type.validate(item)
+                except ValidationError as err:
+                    raise ValidationError('item #%d, %s' % (i, err),
+                                          position=value.position)
+                else:
+                    validated_list.append(item)
+            return ConfigValue(value.name, validated_list, position=value.position)
+
+
+class Section(Container):
+
+    """ A section container used to store a mapping between name and other
+        containers.
+
+        :param \*\*kwargs: parameters used to override meta
+    """
+
+    # Defaults meta:
+    _meta = {'args': None,
+             'unique': False,
+             'repeat': once,
+             'allow_unknown': False}
+
+    def __init__(self, **kwargs):
+        self.meta = {}
+        self.keys = {}
+        for cls in reversed(self.__class__.mro()):
+            # Update meta from class:
+            if hasattr(cls, '_meta'):
+                self.meta.update(cls._meta)
+            # Update fields from class:
+            for key, value in cls.__dict__.iteritems():
+                if isinstance(value, Container):
+                    self.keys[key] = value
+
+    def add(self, name, container):
+        """ Add a new key imperatively.
+
+        :param name: the key name to add
+        :param container: the container to add
+        """
+
+        if name not in self.keys:
+            self.keys[name] = container
+        else:
+            raise KeyError('key already exists')
+
+    def populate_argparse(self, parser, name=None):
+        """ Populate an argparse parser.
+        """
+
+        for name, container in self.keys.iteritems():
+            container.populate_argparse(parser, name=name)
+
+    def validate(self, section):
+        if not isinstance(section, ConfigSection):
+            raise ValidationError('Not a section')
+
+        # Rebuild the section using schema:
+        validated_section = ConfigSection(section.name, section.parent,
+                                          position=section.position)
+        # Validate the section's argument:
+        if self.meta['args'] is None and section.args is not None:
+            raise ValidationError('section %s, this section does not take '
+                                  'any argument' % section.name,
+                                  position=section.args.position)
+        elif self.meta['args'] is not None:
+            try:
+                validated_args = self.meta['args'].validate(section.args_raw)
+            except ValidationError as err:
+                msg = 'section %s, arguments, %s' % (section.name, err)
+                raise ValidationError(msg, position=err.position)
+            else:
+                validated_section.args = validated_args
+        # Validate the section's children:
+        for name, container in self.keys.iteritems():
+            if isinstance(container, Section):
+                # Validate subsections of this section:
+                subsections = list(section.subsections(name))
+                # Check for repeat option:
+                rmin, rmax = container.meta['repeat']
+                if rmax is not None and rmin > rmax:
+                    raise ValidationError('section %s, rmin > rmax' % name)
+                if len(subsections) < rmin:
+                    raise ValidationError('section %s, section must be defined'
+                                          ' at least %d times' % (name, rmin))
+                if rmax is not None and len(subsections) > rmax:
+                    raise ValidationError('section %s, section must be defined'
+                                          ' at max %d times' % (name, rmax))
+                # Do the children validation:
+                args = set()  # Store the already seen args
+                for subsection in subsections:
+                    # Check for unique option:
+                    if container.meta['unique']:
+                        if tuple(subsection.args) in args:
+                            msg = 'section %s, section must be unique' % name
+                            raise ValidationError(msg, position=subsection.position)
+                        else:
+                            args.add(tuple(subsection.args))
+                    # Container validation:
+                    validated_subsection = container.validate(subsection)
+                    validated_section.register(validated_subsection, name=name)
+            elif isinstance(container, Container):
+                # Validate all other types of containers:
+                try:
+                    validated_value = container.validate(section.get(name, raw=False))
+                except ValidationError as err:
+                    raise ValidationError('section %s, key %s, %s' % (section.name, name, err),
+                                          position=err.position)
+                else:
+                    validated_section.register(validated_value, name=name)
+        # Handle the allow_unknown meta option:
+        for name, child in section.iteritems():
+            if name not in validated_section:
+                if self.meta['allow_unknown']:
+                    validated_section.register(child, name=name)
+                else:
+                    msg = 'section %s, unknown key %s' % (section.name, name)
+                    raise ValidationError(msg, position=child.position)
+        return validated_section
