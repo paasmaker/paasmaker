7dfe9cd7aa3f090db84c2454944cd6336ba2ff6a
Daniel Foote <freefoote@dview.net>
2012-08-31 00:28:22 +0800

    Added base service class, to allow for more thinking about it's structure.

---
+++ paasmaker/service/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Base service.
+class Base():
+	def __init__(self):
+		pass
+
+	def validate(self, options):
+		pass
+
+	def get_options(self):
+		pass
+
+	def create(self, options):
+		pass
+
+	def remove(self, options):
+		pass
+
