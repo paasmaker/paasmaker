f62954d7b8f47d44292e5582bdfc0f23509e41d1
Daniel Foote <freefoote@dview.net>
2012-08-31 02:57:42 +0800

    Changed the application configuration to use the yaml parser instead of the dotconf parser. The main configuration is next.

--- paasmaker/application/configuration.py
+++ paasmaker/application/configuration.py
@@ -1,105 +1,84 @@
 #!/usr/bin/env python
 
 import unittest
+import colander
+import yaml
 
-# For parsing configuration files.
-import dotconf
-from dotconf.schema.containers import Section, Value, List
-from dotconf.schema.types import Boolean, Integer, Float, String, Regex
-from dotconf.parser import DotconfParser, yacc, ParsingError
+class Runtime(colander.MappingSchema):
+	name = colander.SchemaNode(colander.String(),
+		title="Runtime name",
+		description="Runtime symbolic name")
+	version = colander.SchemaNode(colander.String(),
+		title="Runtime version",
+		description="The version of the runtime to use.")
 
-# Configuration schema.
-class RuntimeSection(Section):
-	# Required section.
-	_meta = { 'repeat': (1, 1), 'args': Value(String()) }
-	# Runtime version. Required.
-	version = Value(String())
+class Service(colander.MappingSchema):
+	name = colander.SchemaNode(colander.String(),
+		title="Service name",
+		description="Your name for the service to identify it")
+	provider = colander.SchemaNode(colander.String(),
+		title="Provider name",
+		description="Provider symbolic name")
+	options = colander.SchemaNode(colander.Mapping(unknown='preserve'), missing={}, default={})
 
-class ServiceSection(Section):
-	# Optional section. But you can have as many as you'd like.
-	_meta = { 'repeat': (0, None), 'args': Value(String()) }
-	# Provider name. Required.
-	provider = Value(String())
+class Services(colander.SequenceSchema):
+	service = Service(unknown='preserve')
 
-class PlacementSection(Section):
-	# Optional section. And you can only have one.
-	_meta = { 'repeat': (0, 1), 'args': Value(String()) }
+class Placement(colander.MappingSchema):
+	strategy = colander.SchemaNode(colander.String(),
+		title="Placement strategy",
+		description="The placement strategy to use")
 
-class MainSection(Section):
-	# Hostnames for this application.
-	hostnames = List(String())
+	@staticmethod
+	def default():
+		return {'strategy': 'default'}
 
-	# Runtime information.
-	runtime = RuntimeSection()
-
-	# Services information.
-	service = ServiceSection()
-
-	# Placement information.
-	placement = PlacementSection()
+class ConfigurationSchema(colander.MappingSchema):
+	hostnames = colander.SchemaNode(colander.Sequence(), colander.SchemaNode(colander.String(), title="Hostname"))
+	services = Services()
+	runtime = Runtime()
+	placement = Placement(default=Placement.default(), missing=Placement.default())
 
 class InvalidConfigurationException(Exception):
 	pass
 
 class ApplicationConfiguration():
-	def __init__(self, application_configuration):
-		parser = DotconfParser(application_configuration, debug=False, write_tables=False, errorlog=yacc.NullLogger())
-		try:
-			config = parser.parse()
-		except ParsingError, ex:
-			raise InvalidConfigurationException("Invalid configuration file syntax: %s" % str(ex))
-		except AttributeError, ex:
-			raise InvalidConfigurationException("Parser error - probably invalid configuration file syntax. %s" % str(ex))
-		schema = MainSection()
-		try:
-			self.values = schema.validate(config)
-		except dotconf.schema.ValidationError, ex:
-			raise InvalidConfigurationException("Configuration is invalid: %s" % str(ex))
+	def __init__(self, raw):
+		# Convert to Yaml.
+		self.parsed = yaml.load(raw)
+		# Load the schema.
+		schema = ConfigurationSchema()
+		# Parse the structure.
+		self.values = schema.deserialize(self.parsed)
+		# And flatten.
+		self.flat = schema.flatten(self.values)
 
-	def get_section(self, section):
-		"""
-		Simple helper to fetch a section. Assumes only one section, and
-		that the section exists.
-		"""
-		return self.values._subsections[section][0]
+	def get_flat(self, key):
+		return self.flat[key]
 
-	def get_sections(self, section):
-		"""
-		Simple helper to fetch a set of sections.
-		"""
-		return self.values._subsections[section]
-
-	def get_runtime(self):
-		return self.get_section('runtime').args
-
-	def get_runtime_version(self):
-		return self.get_section('runtime').get('version')
-
-	def get_hostnames(self):
-		return self.values.get('hostnames')
-
-	def get_services(self):
-		return self.get_sections('service')
+	def get_key(self, key):
+		return self.values[key]
 
 class TestApplicationConfiguration(unittest.TestCase):
 	test_config = """
-runtime "PHP" {
-	version = "5.4"
-}
+runtime:
+  name: PHP
+  version: 5.4
 
-hostnames = "foo.com", "foo.com.au",
-		"www.foo.com", "www.foo.com.au"
+hostnames:
+  - foo.com
+  - foo.com.au
+  - www.foo.com.au
+  - www.foo.com
 
-service "name" {
-	provider = "postgres"
-	provider_arguments {
-		one = "foo"
-	}
-}
+services:
+  - name: test
+    provider: one
+    options:
+      bar: foo
 
-placement "strategy" {
-	argument = "test"
-}
+placement:
+  strategy: default
 """
 
 	bad_config = """
@@ -113,17 +92,17 @@ placement "strategy" {
 
 	def test_loading(self):
 		config = ApplicationConfiguration(self.test_config)
-		self.assertEquals(config.get_runtime(), "PHP", "Runtime value is not as expected.")
-		self.assertEquals(config.get_runtime_version(), "5.4", "Runtime version is not as expected.")
-		self.assertEquals(len(config.get_hostnames()), 4, "Number of hostnames is not as expected.")
-		self.assertIn("www.foo.com.au", config.get_hostnames(), "Hostnames does not contain an expected item.")
-		self.assertEquals(len(config.get_services()), 1, "Services array does not contain the expected number of items.")
+		self.assertEquals(config.get_flat('runtime.name'), "PHP", "Runtime value is not as expected.")
+		self.assertEquals(config.get_flat('runtime.version'), "5.4", "Runtime version is not as expected.")
+		self.assertEquals(len(config.get_key('hostnames')), 4, "Number of hostnames is not as expected.")
+		self.assertIn("www.foo.com.au", config.get_key('hostnames'), "Hostnames does not contain an expected item.")
+		self.assertEquals(len(config.get_key('services')), 1, "Services array does not contain the expected number of items.")
 
 	def test_bad_config(self):
 		try:
 			config = ApplicationConfiguration(self.bad_config)
 			self.assertTrue(False, "Should have thrown an exception.")
-		except InvalidConfigurationException, ex:
+		except colander.Invalid, ex:
 			self.assertTrue(True, "Threw exception correctly.")
 
 if __name__ == '__main__':
