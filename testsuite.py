e3632971c6bfcff796350d3b746ca58068a9b44e
Daniel Foote <freefoote@dview.net>
2012-09-22 17:41:23 +0800

    Refactored the test runner to allow a selection of tests to be run by tags. Kinda crude but effective.

--- testsuite.py
+++ testsuite.py
@@ -3,45 +3,59 @@
 import logging
 import unittest
 import paasmaker
+import sys
 
 # Suppress log messages.
 # Turning this off temporarily can be helpful for debugging.
 #logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.CRITICAL)
 
+# Define the tests to run. The keys are the modules to search
+# in for test cases, and the array is a set of tags that will
+# cause this test to be run. To keep the code as generic as possible,
+# 'all' is included in each one. 'all' is also the default.
+test_sets = {
+	paasmaker.util.example: ['all', 'quick', 'example'],
+	paasmaker.util.jsonencoder: ['all', 'quick', 'util', 'jsonencoder'],
+	paasmaker.util.configurationhelper: ['all', 'quick', 'util', 'configuration'],
+	paasmaker.util.joblogging: ['all', 'slow', 'util', 'job'],
+	paasmaker.util.port: ['all', 'slow', 'util', 'network'],
+	paasmaker.util.plugin: ['all', 'quick', 'util', 'configuration'],
+	paasmaker.util.commandsupervisor: ['all', 'slow', 'util', 'process'],
+	paasmaker.util.memoryrabbitmq: ['all', 'slow', 'util', 'messaging', 'service'],
+	paasmaker.util.popen: ['all', 'slow', 'util', 'process'],
+
+	paasmaker.configuration.configuration: ['all', 'quick', 'configuration'],
+	paasmaker.application.configuration: ['all', 'quick', 'configuration'],
+
+	paasmaker.model: ['all', 'slow', 'model'],
+
+	paasmaker.controller.example: ['all', 'quick', 'controller'],
+	paasmaker.controller.information: ['all', 'quick', 'controller'],
+	paasmaker.controller.log: ['all', 'slow', 'controller'],
+	paasmaker.controller.login: ['all', 'slow', 'controller'],
+
+	paasmaker.heart.runtime: ['all', 'slow', 'heart', 'runtime'],
+	paasmaker.heart.runtime.php: ['all', 'slow', 'heart', 'runtime', 'php']
+}
+
 if __name__ == '__main__':
-	# Example unit test first.
-	suite = unittest.TestLoader().loadTestsFromModule(paasmaker.util.example)
+	suite = None
 
-	# Then the utilities.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.jsonencoder))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.configurationhelper))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.joblogging))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.port))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.plugin))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.commandsupervisor))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.memoryrabbitmq))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.util.popen))
+	selected = ['all']
+	if len(sys.argv) > 1:
+		selected = sys.argv[1].split(',')
 
-	# Configuration system.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.configuration.configuration))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.application.configuration))
+	print "Selecting unit tests with tags: %s" % str(selected)
 
-	# Database model.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.model))
-
-	# Controllers.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.controller.example))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.controller.information))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.controller.log))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.controller.login))
-
-	# Router.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.router.router))
-
-	# Runtimes.
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.heart.runtime))
-	suite.addTests(unittest.TestLoader().loadTestsFromModule(paasmaker.heart.runtime.php))
+	selected = set(selected)
+	for module, tags in test_sets.iteritems():
+		if len(set(tags).intersection(selected)) > 0:
+			if not suite:
+				suite = unittest.TestLoader().loadTestsFromModule(module)
+			else:
+				suite.addTests(unittest.TestLoader().loadTestsFromModule(module))
 
 	# And run them.
+	print "About to run %d tests." % suite.countTestCases()
 	unittest.TextTestRunner(verbosity=2).run(suite)
