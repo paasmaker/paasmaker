c95668d1ccf252efac708097a7ffad14817944e3
Daniel Foote <freefoote@dview.net>
2012-10-23 15:09:54 +0800

    Added a TODO to the job manager.

--- paasmaker/util/jobmanager.py
+++ paasmaker/util/jobmanager.py
@@ -10,6 +10,10 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+# TODO: To the job manager, add the ability to add jobs but not allow them to execute just yet.
+# Almost like a transaction - this prevents us half adding a job tree and then having that kicked off
+# part way through.
+
 class JobRunner(object):
 	"""
 	Mixin for other classes to make them into job runnable objects.
