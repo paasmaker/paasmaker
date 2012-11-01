d07c52c840e07b65e3ccb5c0a95b99f3bd5b618a
Daniel Foote <freefoote@dview.net>
2012-11-01 11:17:29 +0800

    Added an environment dump action to the simple tornado sample app.

--- misc/samples/tornado-simple/app.py
+++ misc/samples/tornado-simple/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import os
+import json
+
 # From http://www.tornadoweb.org/documentation/overview.html, with a few mods.
 import tornado.ioloop
 import tornado.web
@@ -11,8 +14,25 @@ class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Hello, world")
 
+class EnvironHandler(tornado.web.RequestHandler):
+	def get(self):
+		result = '<pre>'
+		for key, value in os.environ.iteritems():
+			result += tornado.escape.xhtml_escape(key) + ' = '
+			if len(value) > 0 and value[0] == '{':
+				# We're assuming this is JSON... so decode and print it.
+				decoded = json.loads(value)
+				result += tornado.escape.xhtml_escape(json.dumps(decoded, sort_keys=True, indent=4))
+			else:
+				result += tornado.escape.xhtml_escape(value)
+			result += "\n"
+		result += '</pre>'
+
+		self.write(result)
+
 application = tornado.web.Application([
 	(r"/", MainHandler),
+	(r"/environ", EnvironHandler),
 ])
 
 tornado.options.parse_command_line()
