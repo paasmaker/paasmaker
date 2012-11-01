f708820707c72677fc74c57ee3afac39373339cd
Daniel Foote <freefoote@dview.net>
2012-11-01 08:28:44 +0800

    Worked out more of the default placement provider, so it now can select nodes based on runtimes and versions.

--- paasmaker/pacemaker/placement/base.py
+++ paasmaker/pacemaker/placement/base.py
@@ -25,12 +25,15 @@ class BasePlacement(paasmaker.util.plugin.Plugin):
 
 		return result_list
 
-	def filter_by_tags(self, nodes, tags):
+	def filter_by_tags(self, nodes, node_tag_set, tags):
 		"""
 		Filter the given node list by the supplied tags dict.
 		To match, the node must have all the tags present in
 		the supplied tags and they must be equal. If the tags
 		supplied is empty, then all nodes are returned.
+		If a tag value on the node side is a list, it matches
+		if the supplied tags value is contained in that list - which
+		is generally used to match up runtimes.
 		"""
 		# Short circuit: return the nodes if no tags supplied.
 		if len(tags.keys()) == 0:
@@ -42,16 +45,36 @@ class BasePlacement(paasmaker.util.plugin.Plugin):
 		for node in nodes:
 			# Figure out which keys we have in common.
 			node_tags = node.tags
+
+			# Node tags have several top level tags
+			# which we need to switch between. If they don't
+			# exist, assume it's empty, which almost certainly means
+			# that matching will fail.
+			if node_tags.has_key(node_tag_set):
+				node_tags = node_tags[node_tag_set]
+			else:
+				node_tags = {}
+
 			their_keys = set(node_tags.keys())
 
+			# Find the keys in both sets...
 			intersect = our_keys.intersection(their_keys)
 
+			# If the application's tags are all present in
+			# the nodes tags, we can then compare the tags.
+			# If not, ignore it, because the node might not
+			# be specific enough to match this application.
+			# TODO: Explain this better...
 			if len(intersect) == len(our_keys):
 				# Can be a match. Check the keys.
 				matches = True
 				for key in intersect:
-					if tags[key] != node_tags[key]:
-						matches = False
+					if isinstance(node_tags[key], list):
+						if tags[key] not in node_tags[key]:
+							matches = False
+					else:
+						if tags[key] != node_tags[key]:
+							matches = False
 
 				if matches:
 					result_list.append(node)
@@ -89,8 +112,75 @@ class BasePlacementTest(tornado.testing.AsyncTestCase):
 		self.nodes = None
 		self.stop()
 
+	def create_sample_application(self, session, runtime_name, runtime_parameters, runtime_version):
+		workspace = paasmaker.model.Workspace()
+		workspace.name = 'Test'
+
+		application = paasmaker.model.Application()
+		application.workspace = workspace
+		application.name = 'foo.com'
+
+		application_version = paasmaker.model.ApplicationVersion()
+		application_version.application = application
+		application_version.version = 1
+		application_version.is_current = False
+		application_version.manifest = ''
+
+		instance_type = paasmaker.model.ApplicationInstanceType()
+		instance_type.application_version = application_version
+		instance_type.name = 'web'
+		instance_type.quantity = 1
+		instance_type.runtime_name = runtime_name
+		instance_type.runtime_parameters = runtime_parameters
+		instance_type.runtime_version = runtime_version
+		instance_type.startup = ''
+		instance_type.placement_provider = 'paasmaker.placement.default'
+		instance_type.placement_parameters = {}
+		instance_type.exclusive = False
+		instance_type.state = constants.INSTANCE_TYPE.PREPARED
+
+		session.add(instance_type)
+		session.commit()
+
+		return instance_type
+
 	def create_sample_nodes(self, session, quantity):
 		test_items = []
+		tagsets = [
+			{
+				'node': {
+					'foo': 'bar',
+					'test': 'three'
+				},
+				'runtimes': {
+					'paasmaker.runtime.php': ['5.3', '5.3.10'],
+					# This is malformed, but makes a good test.
+					'paasmaker.runtime.ruby': '1.9.3'
+				}
+			},
+			{
+				'node': {},
+				'runtimes': {}
+			},
+			{
+				'node': {
+					'foo': 'bar',
+					'test': 'three'
+				},
+				'runtimes': {
+					'paasmaker.runtime.php': ['5.4', '5.4.4']
+				}
+			},
+			{
+				'node': {
+					'foo': 'bar',
+					'test': 'three'
+				},
+				'runtimes': {
+					'paasmaker.runtime.shell': ['1']
+				}
+			}
+		]
 		ctr = 0
 		for i in range(quantity):
 			ctr += 1
@@ -101,6 +191,7 @@ class BasePlacementTest(tornado.testing.AsyncTestCase):
 					uuid='%s-uuid' % ctr,
 					state=constants.NODE.ACTIVE)
 			n.heart = True
+			n.tags = tagsets[(ctr - 1) % len(tagsets)]
 			test_items.append(n)
 			ctr += 1
 			# Active pacemaker node.
@@ -110,6 +201,7 @@ class BasePlacementTest(tornado.testing.AsyncTestCase):
 					uuid='%s-uuid' % ctr,
 					state=constants.NODE.ACTIVE)
 			n.pacemaker = True
+			n.tags = tagsets[(ctr - 1) % len(tagsets)]
 			test_items.append(n)
 			ctr += 1
 			# Inactive heart node.
@@ -119,16 +211,18 @@ class BasePlacementTest(tornado.testing.AsyncTestCase):
 					uuid='%s-uuid' % ctr,
 					state=constants.NODE.INACTIVE)
 			n.heart = True
+			n.tags = tagsets[(ctr - 1) % len(tagsets)]
 			test_items.append(n)
 			ctr += 1
-			# Active heard node with test tags.
+			# Active pacemaker and heart node.
 			n = Node(name='test%d' % ctr,
 					route='%d.test.paasmaker.com' % ctr,
 					apiport=888,
 					uuid='%s-uuid' % ctr,
 					state=constants.NODE.ACTIVE)
 			n.pacemaker = True
-			n.tags = {'foo': 'bar', 'test': 'three'}
+			n.heart = True
+			n.tags = tagsets[(ctr - 1) % len(tagsets)]
 			test_items.append(n)
 
 		session.add_all(test_items)
--- paasmaker/pacemaker/placement/default.py
+++ paasmaker/pacemaker/placement/default.py
@@ -26,26 +26,123 @@ class DefaultPlacement(BasePlacement):
 	OPTIONS_SCHEMA = DefaultPlacementConfigurationSchema()
 	PARAMETERS_SCHEMA = DefaultPlacementParametersSchema()
 
+	def fail(self, nodes, callback, reason):
+		if len(nodes) == 0:
+			callback(reason)
+			return True
+		else:
+			return False
+
 	def choose(self, session, instance_type, quantity, callback, error_callback):
 		# Query active nodes first.
 		nodes = self.get_active_nodes(session)
 
-		# Filter them by the tags supplied.
+		self.logger.info("Stage 1: Found %d active nodes.", len(nodes))
+
+		if self.fail(nodes, error_callback, "No active nodes found to run this instance."):
+			return
+
+		# Filter by the required runtime.
+		runtime_tags = {}
+		runtime_tags[instance_type.runtime_name] = instance_type.runtime_version
+		self.logger.info("Stage 2: Filtering to these runtimes: %s", str(runtime_tags))
+		nodes = self.filter_by_tags(nodes, 'runtimes', runtime_tags)
+		self.logger.info("Stage 2: Found %d nodes that can run this instance.", len(nodes))
+
+		if self.fail(nodes, error_callback, "No nodes can service the runtime %s, version %s." % (instance_type.runtime_name, instance_type.runtime_version)):
+			return
+
+		# Filter them by the user tags supplied.
 		tags = {}
 		if self.parameters.has_key('tags'):
 			tags = self.parameters['tags']
-		nodes = self.filter_by_tags(nodes, tags)
+		self.logger.info("Stage 3: Filtering to nodes with these tags: %s", str(tags))
+		nodes = self.filter_by_tags(nodes, 'node', tags)
+		self.logger.info("Stage 3: Found %d nodes that match these tags.", len(nodes))
 
-		# Now choose quantity number of nodes.
-		nodes = random.sample(nodes, quantity)
+		if self.fail(nodes, error_callback, "No nodes match the supplied tags: %s" % str(tags)):
+			return
+
+		if len(nodes) < quantity:
+			# We need duplicates to make this work.
+			# Pick random samples until we have enough.
+			# TODO: Don't be random... but we're in a difficult spot here.
+			output_nodes = []
+			for i in range(quantity):
+				output_nodes.extend(random.sample(nodes, 1))
+			nodes = output_nodes
+		else:
+			# Now choose quantity number of nodes.
+			# TODO: Random for now - later make it based on the node stats.
+			nodes = random.sample(nodes, quantity)
+
+		self.logger.info("Stage 4: Successfully selected %d nodes." % len(nodes))
 
 		callback(nodes, "Successfully located nodes.")
 
 class DefaultPlacementTest(BasePlacementTest):
 
+	def test_core_tags_filter(self):
+		plugin = DefaultPlacement(self.configuration, paasmaker.util.plugin.MODE.PLACEMENT, {}, {})
+
+		# Sanity check.
+		plugin.check_options()
+		plugin.check_parameters()
+
+		# Test node.
+		ctr = 1
+		n = paasmaker.model.Node(name='tags-test%d' % ctr,
+				route='%d.tags-test.paasmaker.com' % ctr,
+				apiport=888,
+				uuid='%s-tags-uuid' % ctr,
+				state=constants.NODE.ACTIVE)
+		n.heart = True
+		n.tags = {
+			'runtimes': {
+				'paasmaker.runtime.php': ['5.3', '5.3.10']
+			},
+			'node': {
+				'one': 'two'
+			}
+		}
+		nodes = [n]
+
+		# This should return all the nodes, because no qualifying tags are supplied.
+		result = plugin.filter_by_tags(nodes, 'invalid', {})
+		self.assertEquals(len(result), 1, "Should have returned the node.")
+
+		# This should return no nodes, because there are tags that must meet.
+		result = plugin.filter_by_tags(nodes, 'invalid', {'foo': 'bar'})
+		self.assertEquals(len(result), 0, "Should have returned no nodes.")
+
+		# This should return the node, because the value matches.
+		result = plugin.filter_by_tags(nodes, 'node', {'one': 'two'})
+		self.assertEquals(len(result), 1, "Should have returned the node.")
+
+		# This should not return the node, because the value does not matches.
+		result = plugin.filter_by_tags(nodes, 'node', {'one': 'one'})
+		self.assertEquals(len(result), 0, "Should have returned no nodes.")
+
+		# Try to fetch a runtime - one that doesn't exist.
+		result = plugin.filter_by_tags(nodes, 'runtimes', {'paasmaker.runtime.noexist': '1.0'})
+		self.assertEquals(len(result), 0, "Should have returned no nodes.")
+
+		# Try to fetch a runtime that matches.
+		result = plugin.filter_by_tags(nodes, 'runtimes', {'paasmaker.runtime.php': '5.3.10'})
+		self.assertEquals(len(result), 1, "Should have returned the node.")
+		result = plugin.filter_by_tags(nodes, 'runtimes', {'paasmaker.runtime.php': '5.3'})
+		self.assertEquals(len(result), 1, "Should have returned the node.")
+
+		# Try to fetch a runtime that doesn't match in version.
+		result = plugin.filter_by_tags(nodes, 'runtimes', {'paasmaker.runtime.php': '5'})
+		self.assertEquals(len(result), 0, "Should have returned no nodes.")
+		result = plugin.filter_by_tags(nodes, 'runtimes', {'paasmaker.runtime.php': '5.4'})
+		self.assertEquals(len(result), 0, "Should have returned no nodes.")
+
 	def test_simple(self):
 		session = self.configuration.get_database_session()
 		self.create_sample_nodes(session, 10)
+		instance_type = self.create_sample_application(session, 'paasmaker.runtime.php', {}, '5.3')
 
 		plugin = DefaultPlacement(self.configuration, paasmaker.util.plugin.MODE.PLACEMENT, {}, {})
 
@@ -53,7 +150,7 @@ class DefaultPlacementTest(BasePlacementTest):
 		plugin.check_options()
 		plugin.check_parameters()
 
-		plugin.choose(session, None, 1, self.success_callback, self.failure_callback)
+		plugin.choose(session, instance_type, 1, self.success_callback, self.failure_callback)
 
 		self.wait()
 
@@ -61,3 +158,41 @@ class DefaultPlacementTest(BasePlacementTest):
 		self.assertEquals(len(self.nodes), 1, "Placement did not return requested number of nodes.")
 		self.assertTrue(self.nodes[0].heart, "Returned node was not a heart.")
 		self.assertEquals(self.nodes[0].state, constants.NODE.ACTIVE, "Returned node was not active.")
+
+	def test_more_than_available(self):
+		session = self.configuration.get_database_session()
+		self.create_sample_nodes(session, 10)
+		instance_type = self.create_sample_application(session, 'paasmaker.runtime.php', {}, '5.3')
+
+		plugin = DefaultPlacement(self.configuration, paasmaker.util.plugin.MODE.PLACEMENT, {}, {})
+
+		# Sanity check.
+		plugin.check_options()
+		plugin.check_parameters()
+
+		# Request more than we have available.
+		plugin.choose(session, instance_type, 20, self.success_callback, self.failure_callback)
+
+		self.wait()
+
+		self.assertTrue(self.success, "Placement choice was not successful.")
+		self.assertEquals(len(self.nodes), 20, "Placement did not return requested number of nodes.")
+		self.assertTrue(self.nodes[0].heart, "Returned node was not a heart.")
+		self.assertEquals(self.nodes[0].state, constants.NODE.ACTIVE, "Returned node was not active.")
+
+	def test_no_runtime(self):
+		session = self.configuration.get_database_session()
+		self.create_sample_nodes(session, 10)
+		instance_type = self.create_sample_application(session, 'paasmaker.runtime.noexist', {}, '1.0')
+
+		plugin = DefaultPlacement(self.configuration, paasmaker.util.plugin.MODE.PLACEMENT, {}, {})
+
+		# Sanity check.
+		plugin.check_options()
+		plugin.check_parameters()
+
+		plugin.choose(session, instance_type, 1, self.success_callback, self.failure_callback)
+
+		self.wait()
+
+		self.assertFalse(self.success, "Placement was successful but should not have been..")
