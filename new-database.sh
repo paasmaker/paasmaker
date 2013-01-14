669d1d084c86e4df67ec17793339f1a8393ec4d3
Daniel Foote <freefoote@dview.net>
2013-01-14 13:33:01 +0800

    Add a helper script to bootstrap a new database.

---
+++ new-database.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERNAME="$1"
+EMAIL="$2"
+NAME="$3"
+PASSWORD="$4"
+SUPERKEY="$5"
+
+if [ "$5" == "" ];
+then
+	echo "Usage: $0 username email fullname password superkey"
+	exit 1
+fi
+
+./pm-command.py user-create $USERNAME $EMAIL "NAME" $PASSWORD --superkey=$SUPERKEY
+./pm-command.py role-create Administrator ALL --superkey=$SUPERKEY
+./pm-command.py workspace-create Test test {} --superkey=$SUPERKEY
+./pm-command.py role-allocate 1 1 --superkey=$SUPERKEY
+
