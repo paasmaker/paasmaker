cdd0c3ed3e4e8b836253a4601bfb4423cdc184c2
Hourann Bosci <hourannb@dview.net>
2013-01-15 15:30:24 +0800

    Modify the ProcessCheck class with a (probably slower) alternate method that works on OS X as well as Linux, and add some new unit tests.

--- paasmaker/util/processcheck.py
+++ paasmaker/util/processcheck.py
@@ -1,6 +1,8 @@
 
 import os
 import unittest
+import platform
+import subprocess
 
 class ProcessCheck(object):
 	@staticmethod
@@ -11,36 +13,45 @@ class ProcessCheck(object):
 		process, optionally look for a keyword in the command
 		line of the process.
 
-		This only works on Linux at this time.
-
 		:arg int pid: The PID to check for.
 		:arg str|None keyword: The keyword to search for
 			in the command line. It can appear anywhere
 			in the command line.
 		"""
-		# TODO: Obviously, this is linux specific...
-		pid_path = "/proc/%d/cmdline" % pid
 
-		if os.path.exists(pid_path):
-			# Path exists, so now, if supplied, check the keyword.
-			if keyword:
-				# Load the contents of the cmdline file,
-				# and read it's contents, then check it.
-				fp = open(pid_path, 'r')
-				contents = fp.read()
-				fp.close()
-
-				if keyword in contents:
-					return True
+		if platform.system() == 'Linux':
+			pid_path = "/proc/%d/cmdline" % pid
+	
+			if os.path.exists(pid_path):
+				# Path exists, so now, if supplied, check the keyword.
+				if keyword:
+					# Load the contents of the cmdline file,
+					# and read it's contents, then check it.
+					fp = open(pid_path, 'r')
+					contents = fp.read()
+					fp.close()
+					return (keyword in contents)
 				else:
-					return False
+					# No keyword supplied to check, so just say
+					# "yes, it's running"
+					return True
 			else:
-				# No keyword supplied to check, so assume
-				# that it's running.
-				return True
-		else:
-			# Path does not exist, so process does not exist.
-			return False
+				# Path does not exist, so process does not exist.
+				return False
+
+		if platform.system() == 'Darwin':
+			# TODO: this method works on Linux too, but isn't performance-tested
+			ps = subprocess.Popen(["ps", "-p", str(pid), "-o", "command="], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+			output = ps.stdout.read().strip()
+			if output:
+				if keyword:
+					return (keyword in output)
+				else:
+					return True
+			else:
+				return False
+
+
 
 class ProcessCheckTest(unittest.TestCase):
 	def test_simple(self):
@@ -50,10 +61,27 @@ class ProcessCheckTest(unittest.TestCase):
 		self.assertTrue(result, "Our process is not running.")
 
 		result = ProcessCheck.is_running(ourpid, "python")
-		self.assertTrue(result, "Our process is not running.")
+		self.assertTrue(result, "Process is not running or does not have 'python' keyword.")
 
 		result = ProcessCheck.is_running(ourpid, "argh")
-		self.assertFalse(result, "The keyword matched when it should not have.")
+		self.assertFalse(result, "Keyword 'argh' matched on our process when it shouldn't have.")
 
 		result = ProcessCheck.is_running(100000)
-		self.assertFalse(result, "An invalid PID number for Linux existed.")
+		self.assertFalse(result, "Found PID 100000 running even though it's an invalid value.")
+
+	def test_create_and_destroy_process(self):
+		proc = subprocess.Popen("yes", stdout=subprocess.PIPE)
+		pid = proc.pid
+		
+		result = ProcessCheck.is_running(pid)
+		self.assertTrue(result, ("Created process with pid %d but couldn't find it with is_running" % pid))
+		
+		result = ProcessCheck.is_running(pid, "yes")
+		self.assertTrue(result, ("Called 'yes' with pid %d but couldn't match command line with is_running" % pid))
+		
+		proc.kill()
+		unused_output = proc.communicate()
+		
+		result = ProcessCheck.is_running(pid)
+		self.assertFalse(result, ("Terminated process with pid %d but still found it with is_running" % pid))
+		
