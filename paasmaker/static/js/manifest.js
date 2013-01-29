37f69531bc7b403855cd706b89293031a937fd97
Hourann Bosci <hourannb@dview.net>
2013-01-29 15:37:29 +0800

    Style tweaks for the (still very incomplete) manifest editor, and beginnings of an object representation in JavaScript for manifests.

--- paasmaker/static/js/manifest.js
+++ paasmaker/static/js/manifest.js
@@ -4,11 +4,73 @@
  * Application manifest editor
  */
 
+//----------------------------------------
+
+var manifest = {};
+
+manifest.manifest = function() {};
+manifest.manifest.prototype.format = 1;
+manifest.manifest.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item\">"
+		+ "<h3>manifest</h3>"
+		+ "<p>format: 1</p>"
+		+ "</div>"
+	);
+};
+
+manifest.application = function() {};
+manifest.application.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item\">"
+		+ "<h3>manifest</h3>"
+		+ "<p>format: 1</p>"
+		+ "</div>"
+	);
+};
+
+manifest.instances = function() {};
+manifest.instances.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item pm-manifest-instance-list\"></div>"
+	);
+};
+
+manifest.instance = function() {};
+manifest.instance.prototype.name = "";
+manifest.instance.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item pm-manifest-instance\">"
+		+ "<h3>instance: " + this.name + "</h3>"
+		+ "</div>"
+	);
+};
+
+manifest.services = function() {};
+manifest.services.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item pm-manifest-service-list\"></div>"
+	);
+};
+
+manifest.service = function() {};
+manifest.service.prototype.name = "";
+manifest.service.prototype.draw = function(container) {
+	$(container).append(
+		"<div class=\"pm-manifest-item pm-manifest-service\">"
+		+ "<h3>service: " + this.name + "</h3>"
+		+ "</div>"
+	);
+};
+
+//----------------------------------------
+
+
 $(function() {
 	var textarea = {
 		styles: {
-			fontSize: 15,
-			lineHeight: 19
+			fontSize: 13,
+			lineHeight: 15
 		}
 	}
 
@@ -29,15 +91,20 @@ $(function() {
 
 		var edited_yaml = $("#pm_manifest_yaml_block").val();
 
+		$("#pm_manifest_yaml_form .pm-manifest-error").remove();
+
 		try {
 			console.log(jsyaml.load(edited_yaml));
 		}
 		catch (e) {
 			console.log(e);
+
 			var message = e.problem;
 			if (e.context) { message += e.context; }
 			addErrorMessage(e.problemMark.line, e.problemMark.column, message);
 		}
 	});
+
+
 
 });
