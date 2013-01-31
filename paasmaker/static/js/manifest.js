ddccd0f376b69580c0423944e9bf6cc13c60b3f0
Hourann Bosci <hourannb@dview.net>
2013-01-31 15:40:08 +0800

    Rudimentary rendering in the manifest validator, with new universal plugin format; TODO: refactor application/configuration.py to suit.

--- paasmaker/static/js/manifest.js
+++ paasmaker/static/js/manifest.js
@@ -64,6 +64,122 @@ manifest.service.prototype.draw = function(container) {
 };
 
 //----------------------------------------
+// http://stackoverflow.com/a/6713782
+
+Object.equals = function( x, y ) {
+  if ( x === y ) return true;
+    // if both x and y are null or undefined and exactly the same
+
+  if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+    // if they are not strictly equal, they both need to be Objects
+
+  if ( x.constructor !== y.constructor ) return false;
+    // they must have the exact same prototype chain, the closest we can do is
+    // test there constructor.
+
+  for ( var p in x ) {
+    if ( ! x.hasOwnProperty( p ) ) continue;
+      // other properties were tested using x.constructor === y.constructor
+
+    if ( ! y.hasOwnProperty( p ) ) return false;
+      // allows to compare x[ p ] and y[ p ] when set to undefined
+
+    if ( x[ p ] === y[ p ] ) continue;
+      // if they have the same strict value or identity then they are equal
+
+    if ( typeof( x[ p ] ) !== "object" ) return false;
+      // Numbers, Strings, Functions, Booleans must be strictly equal
+
+    if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+      // Objects and Arrays must be tested recursively
+  }
+
+  for ( p in y ) {
+    if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+      // allows x[ p ] to be set to undefined
+  }
+  return true;
+}
+
+//----------------------------------------
+
+var draw_functions = {
+	manifest: function(structure) {
+		return "<p>I am a manifest, format version: " + structure.format + "</p>";
+	},
+
+	application: function(structure) {
+		var html = "<div class=\"pm-manifest-item pm-manifest-instance\" style=\"border:solid 1px green\">";
+		html += "<h3>application: " + structure.name + "</h3>";
+		html += "</div>";
+		return html;
+	},
+
+	instance: function(structure) {
+		var html = "<div class=\"pm-manifest-item pm-manifest-instance\">";
+		html += "<h3>instance: " + structure.name + "</h3>";
+		if (structure.quantity) {	// TODO: is this required?
+			html += "<p>quantity: " + structure.quantity + "</p>";
+		}
+		if (structure.runtime) {
+			html += draw_functions.plugin(structure.runtime);
+		}
+		if (structure.startup) {
+			if (typeof structure.startup != 'array') {
+				// throw error
+			}
+			structure.startup.forEach(function(list_item) {
+				html += draw_functions.plugin(list_item);
+			});
+		}
+		if (structure.placement) {
+			html += draw_functions.plugin(structure.placement);
+		}
+		html += "</div>";
+		return html;
+	},
+
+	instances: function(structure) {
+		if (typeof structure != 'array') {
+			// throw error
+		}
+
+		var html = "<div style=\"border:solid 1px red\"><h3>Instances</h3>";
+		structure.forEach(function(list_item) {
+			html += draw_functions.instance(list_item);
+		});
+		html += "</div>";
+		return html;
+	},
+
+	plugin: function(structure) {
+		var html = "<div class=\"pm-manifest-item pm-manifest-service\">";
+		if (structure.name) {
+			html += "<h3>plugin: " + structure.name + "</h3>";
+		}
+		if (!structure.plugin) {
+			// throw error 
+		}
+		html += "<p>provider: " + structure.plugin + "</p>";
+		html += "</div>";
+		return html;
+	},
+
+	services: function(structure) {
+		if (typeof structure != 'array') {
+			// throw error
+		}
+
+		var html = "<div style=\"border:solid 1px blue\"><h3>Services</h3>";
+		structure.forEach(function(list_item) {
+			html += draw_functions.plugin(list_item);
+		});
+		html += "</div>";
+		return html;
+	}
+}
+
+//----------------------------------------
 
 
 $(function() {
@@ -73,6 +189,17 @@ $(function() {
 			lineHeight: 15
 		}
 	}
+
+	var last_drawn_manifest;
+
+	var draw = function(structure) {
+		console.log("redrawing")
+		var html = "";
+		for (var key in structure) {
+			html += draw_functions[key](structure[key]);
+		}
+		$("#pm_manifest_rendered").html(html);
+	};
 
 	var addErrorMessage = function(line, column, message) {
 		var error_block = $('#pm_manifest_yaml_form').append(
@@ -84,17 +211,18 @@ $(function() {
 	};
 
 	$("#pm_manifest_yaml_block").on("keyup", function(e, t, o) {
-		//$("#pm_manifest_yaml_block").css(textarea.styles);
-		for (var styleName in textarea.styles) {
-			$("#pm_manifest_yaml_block").css(styleName, textarea.styles[styleName] + "px");
-		}
 
 		var edited_yaml = $("#pm_manifest_yaml_block").val();
 
 		$("#pm_manifest_yaml_form .pm-manifest-error").remove();
 
 		try {
-			console.log(jsyaml.load(edited_yaml));
+			var manifest = jsyaml.load(edited_yaml);
+
+			if (!Object.equals(manifest, last_drawn_manifest)) {
+				draw(manifest);
+				last_drawn_manifest = manifest;
+			}
 		}
 		catch (e) {
 			console.log(e);
@@ -105,6 +233,12 @@ $(function() {
 		}
 	});
 
+	var setUpDisplay = function() {
+		//$("#pm_manifest_yaml_block").css(textarea.styles);
+		for (var styleName in textarea.styles) {
+			$("#pm_manifest_yaml_block").css(styleName, textarea.styles[styleName] + "px");
+		}
+	}
 
-
+	setUpDisplay();
 });
