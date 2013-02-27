5b8da73c28a8a0192a93315dc0bcca48238add97
Daniel Foote <freefoote@dview.net>
2013-02-27 09:09:02 +0800

    Updated the tornado socket.io client to be able to use XHR longpolls if websockets don't work.

--- paasmaker/thirdparty/socketioclient/__init__.py
+++ paasmaker/thirdparty/socketioclient/__init__.py
@@ -3,7 +3,7 @@
 # It was then updated to allow it to work with Tornado, and support XHR Long polls.
 
 from json import dumps, loads
-from time import sleep
+import time
 from urllib import urlopen, quote
 import traceback
 import logging
@@ -20,6 +20,7 @@ __version__ = '0.3-tornado'
 
 
 PROTOCOL = 1  # SocketIO protocol version
+FRAME_SEPARATOR = u'\ufffd'
 
 # TODO:
 # - XHR Longpoll support.
@@ -76,16 +77,103 @@ class WebsocketTransport(WebSocket):
         self.socketio._transport_disconnected()
 
     def on_unsupported(self):
-        pass
+        # Not supported by remote end or something in between.
+        self.socketio._websocket_unsupported()
 
     def send(self, data):
         self.write_message(data)
+
+class LongpollTransport(object):
+
+    def __init__(self, endpoint, io_loop):
+        self.endpoint = endpoint
+        self.io_loop = io_loop
+
+        self.sending = False
+        self.receiving = False
+        self.queue = []
+
+    def configure(self, socketio):
+        self.socketio = socketio
+
+        # Start receiving the first time.
+        self._receive()
+
+    def _receive(self):
+        if self.receiving:
+            return
+
+        def receive_result(response):
+            self.receiving = False
+
+            # Was it an error?
+            if response.code != 200:
+                # It was an error. Wait, and retry again shortly.
+                error_callback = self.socketio.get_callback('', 'connection_error')
+                error_callback(str(response.error))
+
+                self.io_loop.add_timeout(time.time() + 1, self._receive)
+            else:
+                # Got a response. Parse it.
+                packets = response.body.split(FRAME_SEPARATOR)
+                for packet in packets:
+                    self.socketio._recv_packet(packet)
+
+                # And listen again.
+                self.io_loop.add_callback(self._receive)
+
+        self.receiving = True
+        client = tornado.httpclient.AsyncHTTPClient(io_loop=self.io_loop)
+        client.fetch(self.endpoint, receive_result)
+
+    def send(self, data):
+        if self.sending:
+            self.queue.append(data)
+        else:
+            # Send off what we have.
+            self.queue.append(data)
+            self._real_send()
+
+    def _real_send(self):
+        if len(self.queue) > 0:
+            this_send = self.queue
+            self.queue = []
+
+            body = FRAME_SEPARATOR.join(this_send)
+            self.sending = True
+
+            def send_complete(response):
+                self.sending = False
+
+                if response.code != 200:
+                    # It failed. Put everything back on the queue for later.
+                    all_queue = this_send
+                    all_queue.extend(self.queue)
+                    self.queue = all_queue
+
+                    error_callback = self.socketio.get_callback('', 'connection_error')
+                    error_callback(str(response.error))
+
+                    # Try again shortly.
+                    self.io_loop.add_callback(time.time() + 1, self._real_send)
+
+                # Call send again in case something was queued
+                # whilst we were sending.
+                self._real_send()
+
+            client = tornado.httpclient.AsyncHTTPClient(io_loop=self.io_loop)
+            client.fetch(
+                self.endpoint,
+                send_complete,
+                method="POST",
+                body=body
+            )
 
 class SocketIO(object):
 
     messageID = 0
 
-    def __init__(self, host, port, Namespace=BaseNamespace, secure=False, io_loop=None, query=None):
+    def __init__(self, host, port, Namespace=BaseNamespace, secure=False, io_loop=None, query=None, force_longpoll=False):
         self.host = host
         self.port = int(port)
         self.namespace = Namespace(self)
@@ -96,6 +184,8 @@ class SocketIO(object):
 
         self.channelByName = {}
         self.callbackByEvent = {}
+
+        self.force_longpoll = force_longpoll
 
     def __del__(self):
         self.heartbeat_periodic.stop()
@@ -136,7 +226,10 @@ class SocketIO(object):
             error_callback('Websocket not supported by remote.')
             return
 
-        # TODO: Support XHR Longpoll.
+        if self.force_longpoll:
+            self._websocket_unsupported()
+            return
+
         socketURL = '%s://%s/websocket/%s' % (
             'wss' if self.secure else 'ws', self.baseURL, self.sessionID)
         self.connection = WebsocketTransport(socketURL, io_loop=self.io_loop)
@@ -156,6 +249,16 @@ class SocketIO(object):
         # And send one heartbeat to start - this will emit a connect
         # event when it replies.
         self._send_heartbeat()
+
+    def _websocket_unsupported(self):
+        # No websockets. Oh well, let's try again with a long poll instead.
+        longpollURL = '%s://%s/xhr-polling/%s' % (
+            'https' if self.secure else 'http', self.baseURL, self.sessionID)
+        self.connection = LongpollTransport(longpollURL, self.io_loop)
+        self.connection.configure(self)
+
+        # Call _transport_connected(), which sets up the heartbeat,
+        # and sends a heartbeat as well.
 
     def _recv_packet(self, packet):
         # Got a packet from the remote end.
