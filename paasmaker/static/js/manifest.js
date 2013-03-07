4013e030a68b9214c9eb4d4fd2110f0033c63090
Hourann Bosci <hourannb@dview.net>
2013-03-07 15:21:05 +0800

    Various changes to the manifest editor: better error message display, the beginnings of smarter plugin handling, better styling.

--- paasmaker/static/js/manifest.js
+++ paasmaker/static/js/manifest.js
@@ -8,95 +8,79 @@
 
 var manifest = {};
 
-manifest.manifest = function() {};
-manifest.manifest.prototype.format = 1;
-manifest.manifest.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item\">"
-		+ "<h3>manifest</h3>"
-		+ "<p>format: 1</p>"
-		+ "</div>"
-	);
-};
+// manifest.manifest = function() {};
+// manifest.manifest.prototype.format = 1;
+// manifest.manifest.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item\">"
+// 		+ "<h3>manifest</h3>"
+// 		+ "<p>format: 1</p>"
+// 		+ "</div>"
+// 	);
+// };
 
-manifest.application = function() {};
-manifest.application.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item\">"
-		+ "<h3>manifest</h3>"
-		+ "<p>format: 1</p>"
-		+ "</div>"
-	);
-};
+// manifest.application = function() {};
+// manifest.application.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item\">"
+// 		+ "<h3>manifest</h3>"
+// 		+ "<p>format: 1</p>"
+// 		+ "</div>"
+// 	);
+// };
 
-manifest.instances = function() {};
-manifest.instances.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item pm-manifest-instance-list\"></div>"
-	);
-};
+// manifest.instances = function() {};
+// manifest.instances.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item pm-manifest-instance-list\"></div>"
+// 	);
+// };
 
-manifest.instance = function() {};
-manifest.instance.prototype.name = "";
-manifest.instance.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item pm-manifest-instance\">"
-		+ "<h3>instance: " + this.name + "</h3>"
-		+ "</div>"
-	);
-};
+// manifest.instance = function() {};
+// manifest.instance.prototype.name = "";
+// manifest.instance.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item pm-manifest-instance\">"
+// 		+ "<h3>instance: " + this.name + "</h3>"
+// 		+ "</div>"
+// 	);
+// };
 
-manifest.services = function() {};
-manifest.services.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item pm-manifest-service-list\"></div>"
-	);
-};
+// manifest.services = function() {};
+// manifest.services.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item pm-manifest-service-list\"></div>"
+// 	);
+// };
 
-manifest.service = function() {};
-manifest.service.prototype.name = "";
-manifest.service.prototype.draw = function(container) {
-	$(container).append(
-		"<div class=\"pm-manifest-item pm-manifest-service\">"
-		+ "<h3>service: " + this.name + "</h3>"
-		+ "</div>"
-	);
-};
+// manifest.service = function() {};
+// manifest.service.prototype.name = "";
+// manifest.service.prototype.draw = function(container) {
+// 	$(container).append(
+// 		"<div class=\"pm-manifest-item pm-manifest-service\">"
+// 		+ "<h3>service: " + this.name + "</h3>"
+// 		+ "</div>"
+// 	);
+// };
 
 //----------------------------------------
 // http://stackoverflow.com/a/6713782
 
 Object.equals = function( x, y ) {
   if ( x === y ) return true;
-    // if both x and y are null or undefined and exactly the same
-
   if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
-    // if they are not strictly equal, they both need to be Objects
-
   if ( x.constructor !== y.constructor ) return false;
-    // they must have the exact same prototype chain, the closest we can do is
-    // test there constructor.
 
   for ( var p in x ) {
     if ( ! x.hasOwnProperty( p ) ) continue;
-      // other properties were tested using x.constructor === y.constructor
-
     if ( ! y.hasOwnProperty( p ) ) return false;
-      // allows to compare x[ p ] and y[ p ] when set to undefined
-
     if ( x[ p ] === y[ p ] ) continue;
-      // if they have the same strict value or identity then they are equal
-
     if ( typeof( x[ p ] ) !== "object" ) return false;
-      // Numbers, Strings, Functions, Booleans must be strictly equal
-
     if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
-      // Objects and Arrays must be tested recursively
   }
 
   for ( p in y ) {
     if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
-      // allows x[ p ] to be set to undefined
   }
   return true;
 }
@@ -111,20 +95,31 @@ var draw_functions = {
 	application: function(structure) {
 		var html = "<div class=\"pm-manifest-item pm-manifest-instance\" style=\"border:solid 1px green\">";
 		html += "<h3>application: " + structure.name + "</h3>";
+		if (structure.prepare && structure.prepare.commands) {
+			if (typeof structure.prepare.commands != 'array') {
+				// throw error
+			}
+			html += "<h3>Prepare commands</h3>";
+			structure.prepare.commands.forEach(function(list_item) {
+				html += draw_functions.plugin(list_item);
+			});
+		}
 		html += "</div>";
 		return html;
 	},
 
 	instance: function(structure) {
-		var html = "<div class=\"pm-manifest-item pm-manifest-instance\">";
+		var html = "<div class=\"pm-manifest-item pm-manifest-instance\" style=\"border:solid 1px red\">";
 		html += "<h3>instance: " + structure.name + "</h3>";
 		if (structure.quantity) {	// TODO: is this required?
 			html += "<p>quantity: " + structure.quantity + "</p>";
 		}
 		if (structure.runtime) {
+			html += "<h3>Runtime</h3>";
 			html += draw_functions.plugin(structure.runtime);
 		}
 		if (structure.startup) {
+			html += "<h3>Startup plugin</h3>";
 			if (typeof structure.startup != 'array') {
 				// throw error
 			}
@@ -133,6 +128,7 @@ var draw_functions = {
 			});
 		}
 		if (structure.placement) {
+			html += "<h3>Placement plugin</h3>";
 			html += draw_functions.plugin(structure.placement);
 		}
 		html += "</div>";
@@ -144,7 +140,7 @@ var draw_functions = {
 			// throw error
 		}
 
-		var html = "<div style=\"border:solid 1px red\"><h3>Instances</h3>";
+		var html = "<div><h3>Instances</h3>";
 		structure.forEach(function(list_item) {
 			html += draw_functions.instance(list_item);
 		});
@@ -153,14 +149,14 @@ var draw_functions = {
 	},
 
 	plugin: function(structure) {
-		var html = "<div class=\"pm-manifest-item pm-manifest-service\">";
+		var html = "<div class=\"pm-manifest-item pm-manifest-plugin\">";
 		if (structure.name) {
 			html += "<h3>plugin: " + structure.name + "</h3>";
 		}
 		if (!structure.plugin) {
-			// throw error 
+			// throw error
 		}
-		html += "<p>provider: " + structure.plugin + "</p>";
+		html += "<p>provider: <span class=\"pm-bare-plugin-name\">" + structure.plugin + "</span></p>";
 		html += "</div>";
 		return html;
 	},
@@ -170,7 +166,7 @@ var draw_functions = {
 			// throw error
 		}
 
-		var html = "<div style=\"border:solid 1px blue\"><h3>Services</h3>";
+		var html = "<div><h3>Services</h3>";
 		structure.forEach(function(list_item) {
 			html += draw_functions.plugin(list_item);
 		});
@@ -190,28 +186,58 @@ $(function() {
 		}
 	}
 
-	var last_drawn_manifest;
+	var current_manifest;
+	var plugins;
+
+	var findLineNumber = function(label, level) {
+		var spaces_per_tab = 2;
+		var lines = $("#pm_manifest_yaml_block").val().split(/\n/);
+		for (var n = 0; n < lines.length; n++) {
+			if (lines[n].indexOf(label) !== -1 && lines[n].indexOf(label) === spaces_per_tab * level) {
+				return n + 1;
+			}
+		}
+	};
 
 	var draw = function(structure) {
-		console.log("redrawing")
 		var html = "";
 		for (var key in structure) {
+			if(!draw_functions[key]) {
+				var line = findLineNumber(key, 0);
+				throw {
+					problem: "Invalid section type: " + key, // "(should be one of: " + Object.keys(draw_functions).join(', ') + ")",
+					problemMark: { line: line, column: 0 }
+				};
+			}
+
 			html += draw_functions[key](structure[key]);
 		}
 		$("#pm_manifest_rendered").html(html);
+
+		// TODO: do this in CSS?
+		var height = $("#pm_manifest_rendered").height();
+		$("#pm_manifest_yaml_form").css({'height': height + 'px'});
+		$("#pm_manifest_yaml_block").css({'height': height + 'px'});
 	};
 
 	var addErrorMessage = function(line, column, message) {
+		if (line !== null) {
+			var error_top_position = 10 + line * textarea.styles.lineHeight - $('#pm_manifest_yaml_block').scrollTop();
+			var marker = "&#9650; ";
+		} else {
+			var error_top_position = 0;
+			var marker = "&#9642; ";
+		}
+
 		var error_block = $('#pm_manifest_yaml_form').append(
 			"<div class=\"pm-manifest-error\""
-			+ "style=\"top:" + (5 + (line + 1) * textarea.styles.lineHeight) + "px\">"
-			+ message
+			+ "style=\"top:" + error_top_position + "px\">"
+			+ marker + message
 			+ "</div>"
 		);
 	};
 
-	$("#pm_manifest_yaml_block").on("keyup", function(e, t, o) {
-
+	var renderYAML = function() {
 		var edited_yaml = $("#pm_manifest_yaml_block").val();
 
 		$("#pm_manifest_yaml_form .pm-manifest-error").remove();
@@ -219,9 +245,9 @@ $(function() {
 		try {
 			var manifest = jsyaml.load(edited_yaml);
 
-			if (!Object.equals(manifest, last_drawn_manifest)) {
+			if (!Object.equals(manifest, current_manifest)) {
 				draw(manifest);
-				last_drawn_manifest = manifest;
+				current_manifest = manifest;
 			}
 		}
 		catch (e) {
@@ -231,7 +257,7 @@ $(function() {
 			if (e.context) { message += e.context; }
 			addErrorMessage(e.problemMark.line, e.problemMark.column, message);
 		}
-	});
+	}
 
 	var setUpDisplay = function() {
 		//$("#pm_manifest_yaml_block").css(textarea.styles);
@@ -241,4 +267,28 @@ $(function() {
 	}
 
 	setUpDisplay();
+
+	if ($("#pm_manifest_yaml_block").val() != '') {
+		renderYAML();
+	}
+
+	$("#pm_manifest_yaml_block").on("keyup", renderYAML);
+
+	$.getJSON(
+		'/configuration/plugins?format=json',
+		null,
+		function(response) {
+			plugins = response.data.plugins;
+
+			$('.pm-bare-plugin-name').each(function(i, el) {
+				var this_plugin = plugins[el.textContent];
+
+				$(el).replaceWith(
+				    "<select>"
+				    + "<option value=\"" + this_plugin.name + "\">" + this_plugin.title + " (" + this_plugin.name + ")</option>"
+				    + "</select>"
+	            );
+			});
+		}
+	);
 });
