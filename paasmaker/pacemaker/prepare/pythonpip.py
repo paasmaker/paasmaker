5b948164902f12dc86638611823d4cd1934b08ee
Daniel Foote <freefoote@dview.net>
2013-03-15 00:29:15 +0800

    Updates to the Python PIP prepare plugin to try to detect when the requirements file has not changed, and the downloaded packages can be used directly.

--- paasmaker/pacemaker/prepare/pythonpip.py
+++ paasmaker/pacemaker/prepare/pythonpip.py
@@ -5,6 +5,7 @@ import colander
 import time
 import shutil
 import copy
+import hashlib
 
 from base import BasePrepare, BasePrepareTest
 
@@ -40,9 +41,23 @@ class PythonPipPrepare(BasePrepare):
 	def prepare(self, environment, directory, callback, error_callback):
 		# Create or fetch a temporary directory for the download cache.
 		download_cache_path = self.configuration.get_scratch_path_exists(self.called_name)
+		self.logger.info("Download cache path %s", download_cache_path)
 
 		our_environment = copy.deepcopy(environment)
 		our_environment['PIP_DOWNLOAD_CACHE'] = download_cache_path
+
+		# Read the requirements file and create a checksum, to see
+		# if we can satisfy the requirements locally. As in, if you've not
+		# changed the requirements file, it should be able to install directly
+		# from the cache.
+		# TODO: This will have corner cases in it that cause the wrong packages to
+		# be installed. Think about this a little bit more.
+		requirements_fp = open(os.path.join(directory, self.parameters['requirements_name']), 'r')
+		requirements_raw = requirements_fp.read()
+		md5 = hashlib.md5()
+		md5.update(requirements_raw)
+		requirements_sum = md5.hexdigest()
+		requirements_checkfile = os.path.join(download_cache_path, requirements_sum) + '.checkfile'
 
 		# Create a shell script to perform the commands for us.
 		self.tempnam = tempfile.mkstemp()[1]
@@ -64,8 +79,19 @@ class PythonPipPrepare(BasePrepare):
 		# (This also shortens the shebang lines, otherwise they can easily get too long)
 		fp.write("virtualenv --relocatable %s\n" % self.parameters['virtualenv_name'])
 
-		# Install the requriements.
-		fp.write("pip install -r %s\n" % self.parameters['requirements_name'])
+		if not os.path.exists(requirements_checkfile):
+			self.logger.info("New requirements file - downloading packages.")
+			open(requirements_checkfile, 'w')  # This is equivalent to 'touch'.
+
+			# We need to download and cache the packages first.
+			# In theory, this should mean we can reinstall the same packages again
+			# later without touching the internet at all.
+			fp.write("pip install --download %s -r %s\n" % (download_cache_path, self.parameters['requirements_name']))
+		else:
+			self.logger.info("Previously seen requirements file - using existing cache only.")
+
+		# Now install packages from the cache.
+		fp.write("pip install --no-index --find-links=file://%s -r %s\n" % (download_cache_path, self.parameters['requirements_name']))
 
 		fp.close()
 
@@ -130,6 +156,9 @@ class PythonPipPrepareTestTest(BasePrepareTest):
 
 		self.wait()
 
+		#log_file = self.configuration.get_job_log_path('testpreparepythonpip')
+		#print open(log_file, 'r').read()
+
 		self.assertTrue(self.success, "Did not prepare properly.")
 		self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'test')), "Virtualenv does not exist.")
 		self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'test', 'bin', 'activate')), "Virtualenv does not exist.")
@@ -138,12 +167,21 @@ class PythonPipPrepareTestTest(BasePrepareTest):
 		# It should take less time this time because it's cached. (Within 20% anyway)
 		# TODO: This isn't very scientific...
 		shutil.rmtree(os.path.join(self.tempdir, 'test'))
+		self.assertTrue(os.path.exists(self.tempdir))
+
+		logger2 = self.configuration.get_job_logger('testpreparepythonpip2')
+		plugin = self.registry.instantiate(
+			'paasmaker.prepare.pythonpip',
+			paasmaker.util.plugin.MODE.PREPARE_COMMAND,
+			parameters,
+			logger2
+		)
 
 		start = time.time()
 		plugin.prepare(os.environ, self.tempdir, self.success_callback, self.failure_callback)
 		second_time = time.time() - start
 
-		#log_file = self.configuration.get_job_log_path('testpreparepythonpip')
+		#log_file = self.configuration.get_job_log_path('testpreparepythonpip2')
 		#print open(log_file, 'r').read()
 
 		self.assertTrue(self.success, "Did not prepare properly.")
@@ -151,4 +189,4 @@ class PythonPipPrepareTestTest(BasePrepareTest):
 		#self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'test')), "Virtualenv does not exist.")
 		#self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'test', 'bin', 'activate')), "Virtualenv does not exist.")
 
-		self.assertTrue((first_time * 1.2) > second_time, "Was not quicker on second run.")
+		self.assertTrue((first_time * 1.2) > second_time, "Was not quicker on second run (first %0.3f, second %0.3f)." % (first_time, second_time))
