b6421e30aaf869d2a195b3401e4c6dbcf604e88b
Hourann Bosci <hourannb@dview.net>
2013-03-17 15:09:28 +0000

    Basic manager code for HTML5 history; pm.history.js will contain a basic list of routes that can be served client-side, and (should) page reloads whenever the user clicks on any link pointing to one of those URLs. Also includes a crude loading spinner, stolen from http://commons.wikimedia.org/wiki/File:Ajax-loader.gif

---
+++ paasmaker/static/js/pm.history.js
@@ -0,0 +1,61 @@
+/**
+ * Paasmaker 0.9.0
+ * Insert licence notice here
+ *
+ * pm.history.js - manage browser history and ajax versus page reload links
+ */
+
+if (!window.pm) { var pm = {}; }	// TODO: module handling
+
+// Mapping of URLs that can be handled by the JavaScript code (i.e. without a page refresh)
+pm.routingTable = {
+
+};
+
+pm.history = (function() {
+	var current_address;
+
+	return {
+		loadingOverlay: function() {
+			// cover the whole view with a loading spinner
+			var overlay = $("<div class=\"loading-overlay\"><img src=\"/static/img/spinner32.gif\" alt=\"\"></div>");
+			$('#main').append(overlay);
+			overlay.animate({ opacity: 0.8 });		
+		},
+
+		onpopstate: function(e) {
+			// event handler for changes to page history; also runs at page load when the
+			// server-side controller directs us to an empty main.html (i.e. for pages
+			// that are served by handlebars templates instead of tornado templates)
+			if (e.state && e.state.handle_in_js) {
+				var address = document.location.pathname;
+				if (address == current_address) { return false; }
+				current_address = address;
+				
+				pm.history.loadingOverlay();
+				pm.routingTable[address].switchTo.apply(pm.routingTable[address]);
+			}
+		},
+
+		init: function() {
+			// At page load, add an event handler for all clicks. If the click was on a link,
+			// check to see if it's pointing to a page that we can serve up without a page
+			// reload. If so, do a pushState so the history event handler will respond.
+			$(document).on('click', function(e) {
+				if (e.target.tagName == 'A') {
+					var parsed_url = e.target.href.match(/^https?\:\/\/[^\/]+(\/.*)$/);
+					if (pm.routingTable[parsed_url[1]]) {
+						e.preventDefault();
+						window.history.pushState({ handle_in_js: true }, '', parsed_url[1]);
+						pm.history.onpopstate({ state: { handle_in_js: true } });
+					}
+				}
+			});
+			
+			window.addEventListener('popstate', pm.history.onpopstate);
+			// if (foo) {
+			// 	pm.history.onpopstate({ state: { handle_in_js: true } });
+			// }
+		}
+	};
+}());
--- paasmaker/static/js/script.js
+++ paasmaker/static/js/script.js
@@ -1,31 +1,15 @@
 
 if (!window.pm) { var pm = {}; }	// TODO: module handling
 
-function testBrowserFeatures(resultContainer)
-{
-	var tests = {
-		"File API": function () { return !!(window.File && window.FileList && window.FileReader); },
-		"Resumable library uploads": function () { var r = new Resumable(); return r.support; },
-		"JSON parsing": function() { return (!!window.JSON && !!JSON.parse); },
-		"History API": function() { return !!(window.history && history.pushState); },
-		"Drag &amp; drop": function() { var div = document.createElement('div'); return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div); },
-		"Web Sockets": function() { return 'WebSocket' in window || 'MozWebSocket' in window; }
-	};
-
-	var resultList = $('<ul></ul>');
-	for (var test in tests) {
-		var labels = { true: 'yes!', false: 'no :-(' };
-		resultList.append($('<li>' + test + ': ' + labels[tests[test]()] + '</li>'));
-	}
-	resultContainer.empty();
-	resultContainer.append('<p>Feature tests for your browser:</p>', resultList);
-}
+// Load steps for the hybrid HTML/ajax web interface
+$(function() {
+	pm.history.init();
+});
 
 $(document).ready(
 	function()
 	{
-		if( $('.workspace-tag-editor').length > 0 )
-		{
+		if( $('.workspace-tag-editor').length > 0 )			{
 			var values = {Tags: JSON.parse($('#pm_workspace_tags').val())};
 
 			console.log(this);
@@ -37,14 +21,33 @@ $(document).ready(
 				},
 				drawproperty: function(opt, json, root, path, key) {
 					children = {};
-		            if (key == 'Tags' && Object.prototype.toString.call(json[key]) == '[object Object]') {
+								if (key == 'Tags' && Object.prototype.toString.call(json[key]) == '[object Object]') {
 						children.item = $('<div>', { 'class': 'item', 'data-path': path });
-	                    children.item.addClass('expanded group-top-level');
-	                    children.property = $(opt.headerElement || '<span class="property group-header">' + key + '</span>');
-	                }
-		            return children;
-		        }
+											children.item.addClass('expanded group-top-level');
+											children.property = $(opt.headerElement || '<span class="property group-header">' + key + '</span>');
+									}
+								return children;
+						}
 			});
+		}
+
+		var testBrowserFeatures = function(resultContainer) {
+			var tests = {
+				"File API": function () { return !!(window.File && window.FileList && window.FileReader); },
+				"Resumable library uploads": function () { var r = new Resumable(); return r.support; },
+				"JSON parsing": function() { return (!!window.JSON && !!JSON.parse); },
+				"History API": function() { return !!(window.history && history.pushState); },
+				"Drag &amp; drop": function() { var div = document.createElement('div'); return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div); },
+				"Web Sockets": function() { return 'WebSocket' in window || 'MozWebSocket' in window; }
+			};
+
+			var resultList = $('<ul></ul>');
+			for (var test in tests) {
+				var labels = { true: 'yes!', false: 'no :-(' };
+				resultList.append($('<li>' + test + ': ' + labels[tests[test]()] + '</li>'));
+			}
+			resultContainer.empty();
+			resultContainer.append('<p>Feature tests for your browser:</p>', resultList);
 		}
 
 		if( $('#test-browser-features').length > 0 ) {
@@ -198,10 +201,10 @@ $(document).ready(
 // From: http://stackoverflow.com/questions/1219860/javascript-jquery-html-encoding
 // TODO: Reconsider if this is appropriate.
 function htmlEscape(str) {
-    return String(str)
-            .replace(/&/g, '&amp;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
+		return String(str)
+						.replace(/&/g, '&amp;')
+						.replace(/"/g, '&quot;')
+						.replace(/'/g, '&#39;')
+						.replace(/</g, '&lt;')
+						.replace(/>/g, '&gt;');
 }
