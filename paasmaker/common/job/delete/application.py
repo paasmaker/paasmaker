ef9d9539e18a22e214e919f197238e060021d68d
Daniel Foote <freefoote@dview.net>
2013-03-25 09:42:41 +0800

    Bug fix for deleting applications if it fails; the session is closed before the final message.

--- paasmaker/common/job/delete/application.py
+++ paasmaker/common/job/delete/application.py
@@ -132,8 +132,8 @@ class ApplicationDeleteServiceJob(BaseJob):
 				self.service.state = constants.SERVICE.ERROR
 				self.session.add(self.service)
 				self.session.commit()
+				self.failed("Plugin with mode SERVICE_DELETE doesn't exist for service %s" % self.service.provider)
 				self.session.close()
-				self.failed("Plugin with mode SERVICE_DELETE doesn't exist for service %s" %self.service.provider)
 				return
 
 			service_plugin = self.configuration.plugins.instantiate(
