c948fad9d9532673562d984bf670c1f0d0275e5c
Daniel Foote <freefoote@dview.net>
2013-03-28 13:15:20 +0800

    Removed old Chef installation files.

--- install/_old/paasmaker/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Paasmaker"
-maintainer_email "freefoote@dview.net"
-license          "TODO: Choose Licence here"
-description      "Installs/Configures paasmaker"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
