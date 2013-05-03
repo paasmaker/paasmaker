0fbd679a75043ef3778368e8f18532125cde5b2a
Daniel Foote <freefoote@dview.net>
2013-05-03 08:36:59 +0800

    Updated the job status viewer to respond to job status changes.

--- paasmaker/static/js/views/layout/jobblock.js
+++ paasmaker/static/js/views/layout/jobblock.js
@@ -35,6 +35,9 @@ define([
 			this.gotJobTreeBinder = _.bind(this.gotJobTree, this);
 			context.streamSocket.on('job.tree', this.gotJobTreeBinder);
 
+			this.gotJobStatusBinder = _.bind(this.gotJobStatus, this);
+			context.streamSocket.on('job.status', this.gotJobStatusBinder);
+
 			// Fetch the initial tree for this job.
 			context.streamSocket.emit('job.tree', this.options.job_id);
 		},
@@ -43,17 +46,19 @@ define([
 		},
 		render: function() {
 			// Render the initial job tree.
-			this.$el.html(this.renderRecurse(this.jobTree));
+			this.$el.html(this.renderRecurse(this.jobTree, true));
 		},
-		renderRecurse: function(root) {
+		renderRecurse: function(root, isRoot) {
 			// Render this level. Direct to a series of DOM
 			// elements.
+			var isFinished = _.indexOf(finishedStates, root.state) != -1;
 			var thisLevel = $(JobBlockTemplate({
 				job: root,
 				statusClassMap: statusClassMap,
 				statusIconMap: statusIconMap,
 				finishedStates: finishedStates,
-				isFinished: _.indexOf(finishedStates, root.state) != -1
+				isFinished: isFinished,
+				isRoot: isRoot
 			}));
 
 			// Record this as one of "my" jobs.
@@ -86,6 +91,36 @@ define([
 
 			this.render();
 		},
+		gotJobStatus: function(job_id, status) {
+			// Is it for us?
+			if (!this.myJobs[job_id]) { return; }
+
+			var titleBlock = $('.title-' + job_id, this.$el);
+			if (titleBlock.length) {
+				var stateLabel = $('span.icon', titleBlock);
+				var stateIcon = $('i', titleBlock);
+
+				stateLabel.attr('class', 'icon label label-' + statusClassMap[status.state]);
+				stateIcon.attr('class', 'icon-white ' + statusIconMap[status.state]);
+			}
+
+			var finished = _.indexOf(finishedStates, job.state) != -1;
+
+			// If the root job finishes, remove the abort button.
+			if (job_id == this.options.job_id) {
+				if (finished ) {
+					// Remove the abort button.
+					$('.abort', this.$el).remove();
+				}
+			}
+
+			// If the job finished, and it wasn't successful, populate the summary box.
+			if (finished && job.state != "SUCCESS") {
+				var summaryBox = $('.particulars-' + job_id + ' .summary', this.$el);
+				summaryBox.text(job.summary);
+				summaryBox.attr('class', 'summary summary-' + job.state);
+			}
+		},
 		viewLog: function(e) {
 			e.preventDefault();
 			var el = $(e.currentTarget);
@@ -108,6 +143,7 @@ define([
 		},
 		destroy: function() {
 			context.streamSocket.removeListener('job.tree', this.gotJobTreeBinder);
+			context.streamSocket.removeListener('job.status', this.gotJobStatusBinder);
 
 			// Destroy any log viewers.
 			_.each(this.logViewers, function(value, key, list) {
